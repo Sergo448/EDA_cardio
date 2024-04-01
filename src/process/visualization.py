a3a99fdff3c743fa45d4fb93d2400fc3dba4f90c
Sergo448 <Sergey.6362@mail.ru>
2024-04-01 14:42:38 +0300

    add notebooks and sone info

--- src/process/visualization.py
+++ src/process/visualization.py
@@ -7,15 +7,73 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from tabulate import tabulate
-import preprocess as prepr
 """ более красивый внешний вид графиков по умолчанию """
 sns.set()
 
-def create_hist_plots_and_save(data: pd.DataFrame, result_path: str) -> None:
+def height_hist_bins_20(data: pd.DataFrame, result_path: str) -> None:
+    
+    name: str = 'height_hist_bins_20.png'
+    path: str = os.path.join(result_path, name)
+    
+    if os.path.exists(path):
+        os.remove(path)
+        
+    fig, axs = plt.subplots(figsize=(12, 4))
+    print(f'$ Building plot for {name}')
+    data['height'].plot.hist(bins=20, legend=name, ax=axs).get_figure().savefig(path)
+    fig.savefig(f"xx{path}")
+    print(f'$  Saving {name} to {path}')
+    return None
+
+def weight_hist_bins_20(data: pd.DataFrame, result_path: str) -> None:
+    
+    name: str = 'weight_hist_bins_20.png'
+    path: str = os.path.join(result_path, name)
+    
+    if os.path.exists(path):
+        os.remove(path)
+    
+    print(f'$ Building plot for {name}')
+    data['weight'].plot.hist(bins=20, legend=name).get_figure().savefig(path)
+
+    print(f'$  Saving {name} to {path}')
+    return None
+        
+def visualize_plots(data: pd.DataFrame, result_path: str, plot: str) -> None:
     """_summary_
 
     Args:
         data (pd.DataFrame): _description_
         result_path (str): _description_
     """
-    return None
+   
+        
+    if plot == 'height_hist_bins_20':
+        
+        name: str = f'{plot}.png'
+        path: str = os.path.join(result_path, name)
+        if os.path.exists(path):
+            os.remove(path)
+        
+        print(f'$ Building plot for {name}')
+        image = data['height'].hist(bins=20, legend=name)
+
+        print(f'$  Saving {name} to {path}')
+        image.get_figure().savefig(path)
+        
+    elif plot == 'weight_hist_bins_20':
+        
+        name: str = f'{plot}.png'
+        path: str = os.path.join(result_path, name)
+        if os.path.exists(path):
+            os.remove(path)
+            
+        print(f'$ Building plot for {name}')
+        image = data['weight'].hist(bins=20, legend=name)
+
+        print(f'$  Saving {name} to {path}')
+        image.get_figure().savefig(path)
+    else:
+        raise ValueError(f'plot param must be in []')
+    
+    
